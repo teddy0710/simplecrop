0e8d291be956ad8e8a9c485e60ee3ecdc7076e06
zhangflg <password123>
2018-01-08 17:47:27 +0800

    添加zhizhu图片选择器

--- app/src/main/java/com/zhangflg/simplecrop/MainActivity.java
+++ app/src/main/java/com/zhangflg/simplecrop/MainActivity.java
@@ -1,13 +1,52 @@
 package com.zhangflg.simplecrop;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
+import com.zhihu.matisse.Matisse;
+import com.zhihu.matisse.MimeType;
+import com.zhihu.matisse.engine.impl.GlideEngine;
+
+import java.util.List;
+
+/**
+ * @author zhangflg
+ */
 public class MainActivity extends AppCompatActivity {
+    private List<Uri> mSelected;
+    private static final int REQUEST_CODE_CHOOSE = 998;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.iv_choose_photos).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Matisse.from(MainActivity.this)
+                        .choose(MimeType.allOf())
+                        .countable(true)
+                        .maxSelectable(9)
+                        .gridExpectedSize(getResources().getDimensionPixelSize(R.dimen.grid_expected_size))
+                        .restrictOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED)
+                        .thumbnailScale(0.85f)
+                        .imageEngine(new GlideEngine())
+                        .forResult(REQUEST_CODE_CHOOSE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_CHOOSE && resultCode == RESULT_OK) {
+            mSelected = Matisse.obtainResult(data);
+            Log.d("Matisse", "mSelected: " + mSelected);
+        }
     }
 }
